8092ee9a0c79d2483f222f78942efa3191990d19
fixXxal <fittefixals5@gmail.com>
2026-02-27 16:51:43 +0500

    Fix Render start command

--- rollari/settings/production.py
+++ rollari/settings/production.py
@@ -4,14 +4,18 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*.onrender.com,*.rollari.com', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = config(
+    'ALLOWED_HOSTS',
+    default='*.onrender.com,*.up.railway.app,*.railway.app,*.rollari.com',
+    cast=lambda v: [s.strip() for s in v.split(',')]
+)
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL'),
-        conn_max_age=600,
-    )
-}
+DATABASE_URL = config('DATABASE_URL', default=None)
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600)
+    }
 
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
